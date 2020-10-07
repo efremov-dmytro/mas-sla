980e4368e6ad68d1dc25fabc8b508d9cbc2fa039
efremov dmytro <efremov@transloyd.com>
2020-10-07 16:36:48 +0300

    main

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+docker-compose down
+#sudo chmod 777 -R ./slave/
+#sudo chmod 777 -R ./master/
+#rm -rf ./master/data/*
+#rm -rf ./slave/data/*
+#docker-compose build
+docker-compose up -d
+sudo chmod 777 -R ./slave/
+sudo chmod 777 -R ./master/
+until docker exec master sh -c 'export MYSQL_PWD=Babina_; mysql -u root -h 127.0.0.1 -pBabina_ -e ";"'
+do
+    echo "Waiting for _master database connection..."
+    sleep 5
+done
+
+priv_stmt='GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%" IDENTIFIED BY "mydb_slave_pwd"; FLUSH PRIVILEGES;'
+docker exec master sh -c "export MYSQL_PWD=Babina_; mysql -u root -h 127.0.0.1 mydb -e '$priv_stmt'"
+
+until docker-compose exec slave sh -c 'export MYSQL_PWD=Babina_; mysql -u root -h 127.0.0.1 -pBabina_ mydb -e ";"'
+do
+    echo "Waiting for _slave database connection..."
+    sleep 5
+done
+
+dip() {
+#     docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
+    docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
+}
+
+MS_STATUS=`docker exec master sh -c 'export MYSQL_PWD=Babina_; mysql -u root -h 127.0.0.1 -e "SHOW MASTER STATUS"'`
+CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
+CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
+
+#start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(dip master)',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE=$CURRENT_LOG,MASTER_LOG_POS=$CURRENT_POS;START SLAVE;"
+start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(dip master)',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd';START SLAVE;"
+start_slave_cmd='export MYSQL_PWD=Babina_; mysql -u root -h 127.0.0.1 -pBabina_ mydb -e "'
+start_slave_cmd+="$start_slave_stmt"
+start_slave_cmd+='"'
+docker exec slave sh -c "$start_slave_cmd"
+
+docker exec slave sh -c "export MYSQL_PWD=Babina_; mysql -u root -h 127.0.0.1 -pBabina_ mydb -e 'SHOW SLAVE STATUS \G'"
