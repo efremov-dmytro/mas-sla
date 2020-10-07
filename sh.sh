e061b022e8574cfb542c5a80e88395f77c57aa99
efremov dmytro <efremov@transloyd.com>
2020-10-07 19:40:28 +0300

    remove unnecessary file

--- sh.sh
+++
@@ -1,3 +0,0 @@
-MS_STATUS=`docker exec master sh -c 'export MYSQL_PWD=Babina_; mysql -u root -h 127.0.0.1 -e "SHOW MASTER STATUS"'`
-CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
-CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
